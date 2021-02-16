a5ae161ddcee5cbf0b29dad24df5d9343392c491
ilya2002 <ilja.2002@rambler.ru>
2021-02-16 12:42:17 +0300

    New

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import firebase from 'firebase/app'
+import firebase from '@firebase/app'
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
